6f4f221971b739c285d6f3431746c08d7c3b8f75
xjiang-ext <xjiang-ext@xjiang-ext.local>
2016-07-19 17:54:31 +0800

    first update

---
+++ test.py
@@ -0,0 +1,16 @@
+import re
+
+ip = raw_input("input your Ip:")
+print "hello"
+print repr(ip)
+def checkip(ip):
+    print ip  
+    p = re.compile('^((25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(25[0-5]|2[0-4]\d|[01]?\d\d?)$')  
+    print ip
+    if p.match(ip):
+        print "yes"
+        return True    
+    else:
+        print "no"       
+        return False
+checkip(ip)
