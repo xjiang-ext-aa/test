99150eb83532a11f5635a97696801c73ace8105e
xjiang-ext <xjiang-ext@xjiang-ext.lan.appannie.com>
2016-08-19 14:53:29 +0800

    change test2

--- test2.py
+++ test2.py
@@ -1,16 +1,10 @@
-import re
 
-ip = raw_input("input your Ip:")
-print "hello"
-print repr(ip)
-def checkip(ip):
-    print ip  
-    p = re.compile('^((25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(25[0-5]|2[0-4]\d|[01]?\d\d?)$')  
-    print ip
-    if p.match(ip):
-        print "yes"
-        return True    
+name = raw_input("input your Name:")
+
+def checkname(name): 
+    if 's' in name:  
+        print 'Your name cotains the letter "s".'
     else:
-        print "no"       
-        return False
-checkip(ip)
+        print 'Your name does not cotains the letter "s".'
+  
+checkname(name)
