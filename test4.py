5091bc96c303410d4bf141af9ba14e180cc697b1
xjiang-ext-aa <xjiang-ext@xjiang-ext.lan.appannie.com>
2016-09-05 14:31:32 +0800

    123

--- test4.py
+++ test4.py
@@ -1,8 +1,6 @@
-#ashajhsak
-import math
-num=raw_input("")
-print num
-def myprint(num):
-    print num
-    return 0
-myprint(num)
+import jenkins
+
+server = jenkins.Jenkins('http://localhost:8080', username='xjiang-ext', password='JIANGxu041419')
+user = server.get_whoami()
+version = server.get_version()
+print('Hello %s from Jenkins %s' % (user['fullName'], version))
