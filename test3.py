c2f04c7b04e62baa0855d5053b22ebbd7724ca51
xjiang-ext-aa <xjiang-ext@xjiang-ext.lan.appannie.com>
2016-09-05 10:41:52 +0800

    modify

--- test3.py
+++ test3.py
@@ -1,7 +1,10 @@
-import math
-num=raw_input("")
-print num
-def myprint(num):
-    print num
-    return 0
-myprint(num)
+import jenkins
+jenkins_server_url='localhost:8080'
+user_id='xjiang-ext'
+api_token='ddceca17aee7b0af56249c8160c02781'
+server=jenkins.Jenkins(jenkins_server_url, username=user_id, password=api_token)
+server.build_job(job_name)
+server.build_job(Test11, parameters=param_dict)
+server.get_job_info(Test11)
+server.get_job_info(Test11)['lastBuild']['1']
+
