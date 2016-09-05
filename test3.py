9183164302739f34a2adbcb67225204041b051a0
xjiang-ext-aa <xjiang-ext@xjiang-ext.lan.appannie.com>
2016-09-05 11:43:10 +0800

    update agagin

--- test3.py
+++ test3.py
@@ -1,10 +1,12 @@
-import jenkins
-jenkins_server_url='localhost:8080'
+import jenkinsapi
+from jenkinsapi.jenkins import Jenkins
+jenkins_server_url='http://localhost:8080'
 user_id='xjiang-ext'
 api_token='ddceca17aee7b0af56249c8160c02781'
-server=jenkins.Jenkins(jenkins_server_url, username=user_id, password=api_token)
-server.build_job(job_name)
-server.build_job(Test11, parameters=param_dict)
-server.get_job_info(Test11)
-server.get_job_info(Test11)['lastBuild']['1']
+job_name='Test11'
+server=Jenkins(jenkins_server_url, username=user_id, password=api_token)
+job = server[job_name]
+lgb = job.get_last_good_build()
+print lgb.get_revision()
+
 
